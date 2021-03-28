ec700e42cc95aaed6c952d3609cc3ab19ca1f73c
Jason <jasonetienne1@gmail.com>
2021-03-28 15:19:18 +0100

    >>> from music.models import Album, Song >>> Album.object.all() Traceback (most recent call last):   File "<console>", line 1, in <module> AttributeError: type object 'Album' has no attribute 'object' >>> Album.objects.all() <QuerySet []> >>> a = Album(artist='Taylor Swift', album_title='Red',genre='Country', album_logo='https://www.google.com/imgres?imgurl=https %3A%2F%2Fquiteirregular.files.wordpress.com%2F2016%2F03%2F1989.jpg&imgrefurl=https%3A%2F%2Fquiteirregular.wordpress.com%2F2016 %2F03%2F02%2Fwildest-dreams-the-pervasive-irony-of-taylor-swifts-1989%2F&tbnid=qF3UO_h4aK3tsM&vet=12ahUKEwjU1Y_LlNPvAhWMTMAKHS -lA5IQMygDegUIARCtAQ..i&docid=RXjhPw5Bddfe0M&w=1920&h=1080&q=taylor%20swift%201984&ved=2ahUKEwjU1Y_LlNPvAhWMTMAKHS-lA5IQMygDeg UIARCtAQ') >>> a.save <bound method Model.save of <Album: Album object (None)>> >>> a.save() >>> a.artist 'Taylor Swift' >>> a.id 1 >>> a.pk 1 >>> b = Album() >>> b.artist = 'Youth Code' >>> b.album_tytle = 'Commitment to Complications' >>> b.genre = 'Industrial' >>> b.album_logo = 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fmedia.pitchfork.com%2Fphotos%2F5929b4c413d197565213ab6e %2F1%3A1%2Fw_600%2Fde91b0ce.jpg&imgrefurl=https%3A%2F%2Fpitchfork.com%2Freviews%2Falbums%2F21732-commitment-to-complications%2 F&tbnid=vRoXfnnx0eEDdM&vet=12ahUKEwijxPPhldPvAhWGS8AKHXnoDUUQMygAegUIARCjAQ..i&docid=Wzu2hbWWGepAwM&w=600&h=600&q=youth%20code %20commitment%20to%20complications&ved=2ahUKEwijxPPhldPvAhWGS8AKHXnoDUUQMygAegUIARCjAQ' >>> b.save() >>> b.artist 'Youth Code' >>> a.genre = 'Pop' >>> a.save() >>> Album.objects.all() <QuerySet [<Album: Album object (1)>, <Album: Album object (2)>]> >>> exit Use exit() or Ctrl-Z plus Return to exit >>>

--- music/models.py
+++ music/models.py
@@ -11,6 +11,10 @@ class Album(models.Model):
     genre = models.CharField(max_length=100)
     album_logo = models.CharField(max_length=1000) # URL that's why length is so big
 
+    def __str__(self):
+        return self.album_title + ' - ' + self.artist
+
+
 
 class Song(models.Model):
     album = models.ForeignKey(Album, on_delete=models.CASCADE)
