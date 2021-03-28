813fdf478e54e66add9189dbdef7a23addcd8f27
Jason <jasonetienne1@gmail.com>
2021-03-28 17:24:06 +0100

    def detail(request, album_id):     return HttpResponse("<h2> Details for Album ID: " + str(album_id) + "</2h>")
    to views.py
    
    	and
    
    def detail(request, album_id):
        return HttpResponse("<h2> Details for Album ID: " + str(album_id) + "</2h>")
    
    to urls.py to link the 2 and create a new page

--- music/urls.py
+++ music/urls.py
@@ -7,5 +7,12 @@ from . import views
 
 
 urlpatterns = [
+    # /music/
     path('', views.index, name='index'),
+
+    # /music/71
+
+
+def detail(request, album_id):
+    return HttpResponse("<h2> Details for Album ID: " + str(album_id) + "</2h>")
 ]
--- music/views.py
+++ music/views.py
@@ -7,3 +7,7 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("<h1>This is the Music app homepage</h1>")
+
+
+def detail(request, album_id):
+    return HttpResponse("<h2> Details for Album ID: " + str(album_id) + "</2h>")
